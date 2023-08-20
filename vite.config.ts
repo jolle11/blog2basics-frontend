cce608a5e8942cdbe0ae14dbdf83f44ac5f5b52d
Jordi Ollé Ballesté <jordiolle11@gmail.com>
2023-08-20 20:44:15 +0200

    Configured Jotai with Vite and added integration with React Query

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
 import react from '@vitejs/plugin-react';
+import jotaiDebugLabel from 'jotai/babel/plugin-debug-label';
+import jotaiReactRefresh from 'jotai/babel/plugin-react-refresh';
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
+	plugins: [
+		react({ babel: { plugins: [jotaiDebugLabel, jotaiReactRefresh] } }),
+	],
+	// ... The rest of your configuration
 });
