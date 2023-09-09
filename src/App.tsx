a8d657acb950ba169d5b63aa182454727b2ad3ba
Jordi Ollé Ballesté <jordiolle11@gmail.com>
2023-09-09 20:38:30 +0200

    Routing, config provider for theming and styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,45 @@
-import "./App.css";
+import Layout, { Content } from "antd/es/layout/layout";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import AppHeader from "./components/AppHeader";
+import AppFooter from "./components/AppFooter";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import LandingPage from "./pages/LandingPage";
+import { ConfigProvider, Divider, theme } from "antd";
+import { useState } from "react";
 
 function App() {
+    const { defaultAlgorithm, darkAlgorithm } = theme;
+    const [isDarkMode, setIsDarkMode] = useState<boolean>(false);
+
     return (
-        <>
-            <h1>App content asdf f hjlasdf ;ljkas df;lkj asf;lkjasdf</h1>
-        </>
+        <ConfigProvider
+            theme={{
+                algorithm: isDarkMode ? darkAlgorithm : defaultAlgorithm,
+                token: {
+                    colorPrimary: "#1463f3",
+                },
+            }}
+        >
+            <Router>
+                <Layout className="app">
+                    <AppHeader theme={isDarkMode} setTheme={setIsDarkMode} />
+                    <Divider style={{ margin: 0 }} />
+                    <Content>
+                        <Routes>
+                            <Route path="/" element={<LandingPage />} />
+                            <Route path="/login" element={<LoginPage />} />
+                            <Route
+                                path="/register"
+                                element={<RegisterPage />}
+                            />
+                        </Routes>
+                    </Content>
+                    <Divider style={{ margin: 0 }} />
+                    <AppFooter />
+                </Layout>
+            </Router>
+        </ConfigProvider>
     );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,10 @@
-import { Content } from "antd/es/layout/layout";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import App from "./App.tsx";
-import Sidebar from "./components/Sider.tsx";
 import "./index.css";
-
-const router = createBrowserRouter([{ path: "/", element: <App /> }]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
-        <Sidebar />
-        <Content style={{ marginLeft: 200 }}>
-            <RouterProvider router={router} />
-        </Content>
+        <App />
     </React.StrictMode>
 );
