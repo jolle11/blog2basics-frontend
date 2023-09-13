b0ec9b8a38e56190e66f4b89d9e9bfc9b6a5c2da
Jordi Ollé Ballesté <jordiolle11@gmail.com>
2023-09-13 21:19:50 +0200

    Imports and deleted svg

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
+import { ConfigProvider, Divider, message, theme } from "antd";
 import Layout, { Content } from "antd/es/layout/layout";
+import { useState } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import AppHeader from "./components/AppHeader";
 import AppFooter from "./components/AppFooter";
+import AppHeader from "./components/AppHeader";
+import LandingPage from "./pages/LandingPage";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
-import LandingPage from "./pages/LandingPage";
-import { ConfigProvider, Divider, theme } from "antd";
-import { useState } from "react";
 
 function App() {
     const { defaultAlgorithm, darkAlgorithm } = theme;
@@ -17,7 +17,7 @@ function App() {
             theme={{
                 algorithm: isDarkMode ? darkAlgorithm : defaultAlgorithm,
                 token: {
-                    colorPrimary: "#1463f3",
+                    colorPrimary: "#246DF5",
                 },
             }}
         >
