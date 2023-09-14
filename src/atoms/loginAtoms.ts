311ba07927cd6e72f35ea1222a3b62cae9ccec4c
Jordi Ollé Ballesté <jordiolle11@gmail.com>
2023-09-14 22:01:31 +0200

    Atoms refactor

--- src/atoms/loginAtoms.ts
+++
@@ -1,6 +0,0 @@
-import { atom } from "jotai";
-
-export const userEmailAtom = atom<string | null>("");
-export const passwordAtom = atom<string | null>("");
-export const isAuthenticatedAtom = atom<boolean | null>(false);
-export const authTokenAtom = atom<string | null>(null);
---
+++ src/atoms/userAtoms.ts
@@ -0,0 +1,7 @@
+import { atom } from 'jotai';
+
+export const userEmailAtom = atom<string | null>('');
+export const userPasswordAtom = atom<string | null>('');
+export const usernameAtom = atom<string | null>('');
+export const userSurnameAtom = atom<string | null>('');
+export const userAliasAtom = atom<string | null>('');
