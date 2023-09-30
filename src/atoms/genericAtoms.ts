2ae9251bbf64294afc7e186314c3a8aa5e087ed1
Jordi Ollé Ballesté <jordiolle11@gmail.com>
2023-09-30 21:06:54 +0200

    Moved isDarkModeAtom

--- src/atoms/genericAtoms.ts
+++ src/atoms/genericAtoms.ts
@@ -1,4 +1,5 @@
 import { atom } from 'jotai';
 
+export const isDarkModeAtom = atom<boolean | null>(false);
 export const isLoadingAtom = atom<boolean | null>(true);
 export const errorAtom = atom<string | null>(null);
