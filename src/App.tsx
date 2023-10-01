5d47b717bbb1371d0aeadd582fb340f2218b6461
Jordi Ollé Ballesté <jordiolle11@gmail.com>
2023-10-01 14:11:34 +0200

    Layout styling and improvement, added new routes and other fixes

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,17 @@ import { ConfigProvider, Divider, theme } from "antd";
 import Layout, { Content } from "antd/es/layout/layout";
 import axios from "axios";
 import { useAtom } from "jotai";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { authTokenAtom, isAuthenticatedAtom } from "./atoms/authAtoms";
 import { userBlogAtom } from "./atoms/blogAtoms";
-import { isLoadingAtom } from "./atoms/genericAtoms";
+import { isAppLoadingAtom, isDarkModeAtom } from "./atoms/genericAtoms";
 import { userAliasAtom } from "./atoms/userAtoms";
 import AppFooter from "./components/AppFooter";
 import AppHeader from "./components/AppHeader";
-import Loader from "./components/Loader";
+import { Loader } from "./components/Loader";
+import BlogPage from "./pages/BlogPage";
+import BlogPostPage from "./pages/BlogPostPage";
 import DashboardPage from "./pages/DashboardPage";
 import LandingPage from "./pages/LandingPage";
 import LoginPage from "./pages/LoginPage";
@@ -18,12 +20,12 @@ import RegisterPage from "./pages/RegisterPage";
 
 function App() {
     const { defaultAlgorithm, darkAlgorithm } = theme;
-    const [isDarkMode, setIsDarkMode] = useState<boolean>(false);
+    const [isDarkMode, setIsDarkMode] = useAtom(isDarkModeAtom);
 
     const [, setAlias] = useAtom(userAliasAtom);
     const [, setBlog] = useAtom(userBlogAtom);
 
-    const [isLoading, setIsLoading] = useAtom(isLoadingAtom);
+    const [isLoading, setIsLoading] = useAtom(isAppLoadingAtom);
 
     const [authToken, setAuthToken] = useAtom(authTokenAtom);
     const [isAuthenticated, setIsAuthenticated] = useAtom(isAuthenticatedAtom);
@@ -31,7 +33,6 @@ function App() {
     useEffect(() => {
         if (localStorage.getItem("authToken")) {
             const persistToken = localStorage.getItem("authToken");
-            console.log(authToken);
             axios
                 .post(
                     "http://localhost:3333/api/user",
@@ -47,7 +48,6 @@ function App() {
                     }
                 )
                 .then((response) => {
-                    console.log(response);
                     setAuthToken(response.data.user.remember_me_token);
                     setIsAuthenticated(true);
                     setAlias(response.data.user.alias);
@@ -55,9 +55,10 @@ function App() {
                     setIsLoading(false);
                 })
                 .catch((error) => {
-                    console.log(error);
                     setIsLoading(false);
                 });
+        } else {
+            setIsLoading(false);
         }
     }, []);
 
@@ -71,36 +72,45 @@ function App() {
             }}
         >
             {isLoading ? (
-                <Loader />
+                <Layout
+                    style={{
+                        display: "flex",
+                        alignItems: "center",
+                        justifyContent: "center",
+                    }}
+                    className="app"
+                >
+                    <Loader size={50} />
+                </Layout>
             ) : (
                 <Router>
                     <Layout className="app">
-                        <>
-                            <AppHeader
-                                theme={isDarkMode}
-                                setTheme={setIsDarkMode}
-                            />
-                            <Divider style={{ margin: 0 }} />
-                            <Content>
-                                <Routes>
-                                    <Route path="/" element={<LandingPage />} />
-                                    <Route
-                                        path="/dashboard"
-                                        element={<DashboardPage />}
-                                    />
-                                    <Route
-                                        path="/login"
-                                        element={<LoginPage />}
-                                    />
-                                    <Route
-                                        path="/register"
-                                        element={<RegisterPage />}
-                                    />
-                                </Routes>
-                            </Content>
-                            <Divider style={{ margin: 0 }} />
-                            <AppFooter />
-                        </>
+                        <AppHeader />
+                        <Divider style={{ margin: 0 }} />
+                        <Content className="content">
+                            <Routes>
+                                <Route path="/" element={<LandingPage />} />
+                                <Route
+                                    path="/dashboard"
+                                    element={<DashboardPage />}
+                                />
+                                <Route path="/login" element={<LoginPage />} />
+                                <Route
+                                    path="/register"
+                                    element={<RegisterPage />}
+                                />
+                                <Route
+                                    path="/blogs/:blogId"
+                                    element={<BlogPage />}
+                                />
+                                <Route
+                                    path="/blogs/:blogId/posts/:postId"
+                                    element={<BlogPostPage />}
+                                />
+                            </Routes>
+                        </Content>
+                        <Divider style={{ margin: 0 }} />
+                        <AppFooter />
                     </Layout>
                 </Router>
             )}
