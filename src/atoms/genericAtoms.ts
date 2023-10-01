4dee2a2293b555285322f6a1c7d16aaddbe1d84c
Jordi Ollé Ballesté <jordiolle11@gmail.com>
2023-10-01 14:12:22 +0200

    Added loading value for the whole app

--- src/atoms/genericAtoms.ts
+++ src/atoms/genericAtoms.ts
@@ -1,5 +1,6 @@
 import { atom } from 'jotai';
 
-export const isDarkModeAtom = atom<boolean | null>(false);
-export const isLoadingAtom = atom<boolean | null>(true);
+export const isDarkModeAtom = atom<boolean>(false);
+export const isLoadingAtom = atom<boolean>(false);
+export const isAppLoadingAtom = atom<boolean>(true);
 export const errorAtom = atom<string | null>(null);
