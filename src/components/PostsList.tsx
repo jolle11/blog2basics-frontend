5b918f19e4e6d0d8b9b7fd3594ed6fb3eeccf17f
Jordi Ollé Ballesté <jordiolle11@gmail.com>
2023-10-01 14:19:14 +0200

    WIP - Posts list

--- src/components/PostsList.tsx
+++ src/components/PostsList.tsx
@@ -1,20 +1,46 @@
+import Space from "antd/es/space";
 import Paragraph from "antd/es/typography/Paragraph";
-import { Post } from "../atoms/blogAtoms";
 import Title from "antd/es/typography/Title";
+import { useNavigate } from "react-router-dom";
+import { Blog, Post } from "../atoms/blogAtoms";
 
 interface Props {
     posts: Post[];
+    blog: Blog;
 }
 
-const PostsList = ({ posts }: Props) => {
-    return posts.map((post) => {
-        return (
-            <>
-                <Title level={4}>{post.title}</Title>
-                <Paragraph>{post.body}</Paragraph>
-            </>
-        );
-    });
+const PostsList = ({ posts, blog }: Props) => {
+    const navigate = useNavigate();
+
+    return (
+        <Space
+            style={{
+                maxWidth: "75vw",
+                display: "flex",
+                flexDirection: "column",
+                margin: "auto",
+            }}
+        >
+            {posts.map((post) => {
+                return (
+                    <Space
+                        style={{
+                            display: "flex",
+                            flexDirection: "column",
+                            margin: "auto",
+                            cursor: "pointer",
+                        }}
+                        onClick={() => {
+                            navigate(`/blogs/${blog.id}/posts/${post.id}`);
+                        }}
+                    >
+                        <Title level={4}>{post.title}</Title>
+                        <Paragraph>{post.body}</Paragraph>
+                    </Space>
+                );
+            })}
+        </Space>
+    );
 };
 
 export default PostsList;
