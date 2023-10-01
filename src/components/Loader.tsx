610b8b631224ec5e608fd55c4af2f356735f3f97
Jordi Ollé Ballesté <jordiolle11@gmail.com>
2023-10-01 14:14:52 +0200

    Changed to ant design loader

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,23 +1,10 @@
-import "../styles/Loader.css";
+import { LoadingOutlined } from "@ant-design/icons";
+import { Spin } from "antd";
 
-interface ILoaderProps {
-    width?: number;
-    height?: number;
-    thickness?: number;
-    borderColor?: string;
+interface Props {
+    size?: number;
 }
 
-const Loader = ({ width = 18, height = 18, thickness = 2 }: ILoaderProps) => {
-    return (
-        <div
-            style={{
-                borderWidth: `${thickness}px`,
-                width: `${width}px`,
-                height: `${height}px`,
-            }}
-            className="loader"
-        ></div>
-    );
+export const Loader = ({ size }: Props) => {
+    return <Spin indicator={<LoadingOutlined style={{ fontSize: size }} />} />;
 };
-
-export default Loader;
