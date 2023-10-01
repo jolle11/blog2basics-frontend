88577bdadfc3ab414bcab0982432ea431bccf2c5
Jordi Ollé Ballesté <jordiolle11@gmail.com>
2023-10-01 14:16:38 +0200

    Added logic, styles and modal tweaks

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -1,16 +1,20 @@
+import Space from "antd/es/space";
+import Paragraph from "antd/es/typography/Paragraph";
 import Title from "antd/es/typography/Title";
 import axios from "axios";
 import { useAtom } from "jotai";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { authTokenAtom } from "../atoms/authAtoms";
 import { userBlogAtom, userPostsAtom } from "../atoms/blogAtoms";
+import NewBlogModal from "../components/NewBlogModal";
 import PostsList from "../components/PostsList";
-import { Content } from "antd/es/layout/layout";
 
 const DashboardPage = () => {
     const [blog, setBlog] = useAtom(userBlogAtom);
     const [authToken] = useAtom(authTokenAtom);
     const [posts, setPosts] = useAtom(userPostsAtom);
+
+    const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
 
     useEffect(() => {
         if (blog) {
@@ -32,25 +36,38 @@ const DashboardPage = () => {
     }, [blog]);
 
     return (
-        <Content
+        <Space
+            className="content"
             style={{
                 display: "flex",
                 flexDirection: "column",
                 alignItems: "center",
-                justifyContent: "center",
-                margin: "auto",
-                height: "100%",
+                justifyContent: "start",
+                padding: "30px",
+                maxWidth: "80vw",
             }}
         >
-            {blog.id !== 0 ? (
+            {blog?.id !== 0 && !!blog?.id ? (
                 <>
-                    <Title>{blog.name}</Title>
-                    <PostsList posts={posts} />
+                    <Title style={{ margin: 0, textAlign: "center" }}>
+                        {blog?.name}
+                    </Title>
+                    <Paragraph italic={true} style={{ textAlign: "center" }}>
+                        {blog?.description}
+                    </Paragraph>
+                    <PostsList posts={posts} blog={blog} />
                 </>
             ) : (
-                <Title>CreateBlog</Title>
+                <>
+                    <Title>Start by creating your blog</Title>
+                    <NewBlogModal
+                        open={isModalOpen}
+                        setOpen={setIsModalOpen}
+                        setBlog={setBlog}
+                    />
+                </>
             )}
-        </Content>
+        </Space>
     );
 };
 
