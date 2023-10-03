41fb49db5682ded37bea950443f4ffbbc226a1de
Jordi Ollé Ballesté <jordiolle11@gmail.com>
2023-10-03 20:09:18 +0200

    Added post and comments

--- src/pages/BlogPostPage.tsx
+++ src/pages/BlogPostPage.tsx
@@ -1,7 +1,21 @@
-import React from "react";
+import { Space } from "antd";
+import CommentsComponent from "../components/CommentsComponent";
+import PostComponent from "../components/PostComponent";
 
 const BlogPostPage = () => {
-    return <div>BlogPostPage</div>;
+    return (
+        <Space
+            style={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "start",
+                justifyContent: "space-between",
+            }}
+        >
+            <PostComponent />
+            <CommentsComponent />
+        </Space>
+    );
 };
 
 export default BlogPostPage;
